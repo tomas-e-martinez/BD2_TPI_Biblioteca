9c40bffab865ad1b14e9e67bdef15e1623c3bd92
fpablogaston <110947048+fpablogaston@users.noreply.github.com>
2025-06-10 16:38:38 -0300

    Update 04_triggers.sql

--- sql/04_triggers.sql
+++ sql/04_triggers.sql
@@ -8,3 +8,16 @@ BEGIN
 	FROM Ejemplares E
 	INNER JOIN inserted I ON E.IDEjemplar = I.IDEjemplar
 END
+
+
+
+CREATE TRIGGER TR_RestaurarEstadoEjemplar
+ON Prestamos
+AFTER DELETE
+AS
+BEGIN
+	UPDATE E
+	SET E.Estado = 'Disponible'
+	FROM Ejemplares E
+	INNER JOIN deleted D ON E.IDEjemplar = D.IDEjemplar
+END
