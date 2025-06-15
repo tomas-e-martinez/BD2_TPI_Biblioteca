a2d51f884ee70b74847698fd563887ac1fd0721c
Tomás Martínez <tomas.emart@gmail.com>
2025-06-14 22:16:27 -0300

    fix: indent

--- sql/02_views.sql
+++ sql/02_views.sql
@@ -1,15 +1,20 @@
 CREATE VIEW VW_PrestamosActivos AS
-SELECT P.IDPrestamo, (U.Nombre + ' ' + U.Apellido) AS Usuario, L.Titulo, P.FechaPrestamo, P.FechaDevolucion,
-CASE
-	WHEN P.FechaDevolucion < GETDATE()
-	THEN 1
-	ELSE 0
-END AS Atrasado,
-CASE
-	WHEN P.FechaDevolucion < GETDATE()
-	THEN DATEDIFF(DAY, P.FechaDevolucion, GETDATE())
-	ELSE NULL
-END AS DiasAtraso
+SELECT 
+	P.IDPrestamo, 
+	(U.Nombre + ' ' + U.Apellido) AS Usuario, 
+	L.Titulo, 
+	P.FechaPrestamo, 
+	P.FechaDevolucion,
+	CASE
+		WHEN P.FechaDevolucion < GETDATE()
+		THEN 1
+		ELSE 0
+	END AS Atrasado,
+	CASE
+		WHEN P.FechaDevolucion < GETDATE()
+		THEN DATEDIFF(DAY, P.FechaDevolucion, GETDATE())
+		ELSE NULL
+	END AS DiasAtraso
 FROM Prestamos P
 INNER JOIN Usuarios U ON P.IDUsuario = U.IDUsuario
 INNER JOIN Ejemplares E ON P.IDEjemplar = E.IDEjemplar
