f1338ad4485870484a7e22ab91831625fd61653a
Tomás Martínez <tomas.emart@gmail.com>
2025-06-17 15:31:28 -0300

    fix: lógica TR_RestaurarEstadoEjemplar

--- sql/04_triggers.sql
+++ sql/04_triggers.sql
@@ -20,6 +20,5 @@ BEGIN
 	SET E.Estado = 'Disponible'
 	FROM Ejemplares E
 	INNER JOIN deleted D ON E.IDEjemplar = D.IDEjemplar
-	INNER JOIN Prestamos P ON P.IDPrestamo = D.IDPrestamo
-	WHERE P.Devuelto = 0 --SOLO ACTUALIZA SI EL PRÉSTAMO NO FUE DEVUELTO (ES DECIR, SE ABORTÓ)
+	WHERE D.Devuelto = 0 --SOLO ACTUALIZA SI EL PRÉSTAMO NO FUE DEVUELTO (ES DECIR, SE ABORTÓ)
 END
