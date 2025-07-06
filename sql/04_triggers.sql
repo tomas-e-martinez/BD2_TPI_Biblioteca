9245b8ed25a81c4a732501658176f9ab185d202a
Tomás Martínez <tomas.emart@gmail.com>
2025-07-06 15:03:16 -0300

    add trigger TR_ActualizarEjemplarPrestamoDevuelto para Prestamo after update

--- sql/04_triggers.sql
+++ sql/04_triggers.sql
@@ -9,8 +9,6 @@ BEGIN
 	INNER JOIN inserted I ON E.IDEjemplar = I.IDEjemplar
 END
 
-
-
 CREATE TRIGGER TR_RestaurarEstadoEjemplar
 ON Prestamos
 AFTER DELETE
@@ -21,4 +19,17 @@ BEGIN
 	FROM Ejemplares E
 	INNER JOIN deleted D ON E.IDEjemplar = D.IDEjemplar
 	WHERE D.Devuelto = 0 --SOLO ACTUALIZA SI EL PRÉSTAMO NO FUE DEVUELTO (ES DECIR, SE ABORTÓ)
+END
+
+CREATE TRIGGER TR_ActualizarEjemplarPrestamoDevuelto
+ON Prestamos
+AFTER UPDATE
+AS
+BEGIN
+	UPDATE E
+	SET E.Estado = 'Disponible'
+	FROM Ejemplares E
+	INNER JOIN inserted I ON E.IDEjemplar = I.IDEjemplar
+	INNER JOIN deleted D ON E.IDEjemplar = D.IDEjemplar
+	WHERE I.Devuelto = 1 AND D.Devuelto = 0
 END
